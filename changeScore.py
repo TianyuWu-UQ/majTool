d9f1c1befea6c8cf66d945845a0a3d4a6083a6ca
TianyuWu-UQ <wty.sky.2001@gmail.com>
2023-06-06 13:20:36 +1000

    working on the sub-interface of "ron" ：add intial choosing score board on it. 6.6

--- changeScore.py
+++ changeScore.py
@@ -2,16 +2,21 @@ from calculation import calculation
 import tkinter as tk
 from tkinter import ttk
 
+winner_player_y = 150
 
 class changeScore:
     def __init__(self, main, cal):
+        global winner_player_y
+        winner_player_y = 150
+
         self.main = main
         self.current_score = cal
         self.root = tk.Toplevel(self.main)
 
         self.style_method = ttk.Style()
         self.style_method.configure('Method.TRadiobutton', font=("Helvetica", 20, "bold"))
-
+        self.style_winner = ttk.Style()
+        self.style_winner.configure('Winner.TRadiobutton', font=("Helvetica", 14, "bold"))
         self.method_choose = tk.StringVar()
         self.method_ryuukyoku = ttk.Radiobutton(self.root, text="流局", variable=self.method_choose, value="ryuukyoku",
                                                 command=self.click_button_ryuukyoku, style='Method.TRadiobutton')
@@ -26,6 +31,82 @@ class changeScore:
         pass
 
     def click_button_ron(self):
+
+
+        def click_winner_self():
+            global winner_player_y
+            label_winner_self = tk.Label(self.root,text="自家",font=("Helvetica", 14, "bold"))
+            label_winner_self.place(x=10,y=winner_player_y)
+            winner_player_y += 25
+            winner_score_board()
+            winner_player_y += 25
+
+        def click_winner_kamicha():
+            global winner_player_y
+            label_winner_kamicha = tk.Label(self.root,text="上家",font=("Helvetica", 14, "bold"))
+            label_winner_kamicha.place(x=10, y=winner_player_y)
+            winner_player_y += 25
+            winner_score_board()
+            winner_player_y += 25
+
+        def click_winner_toimen():
+            global winner_player_y
+            label_winner_toimen = tk.Label(self.root,text="对家",font=("Helvetica", 14, "bold"))
+            label_winner_toimen.place(x=10, y=winner_player_y)
+            winner_player_y += 25
+            winner_score_board()
+            winner_player_y += 25
+
+        def click_winner_shimocha():
+            global winner_player_y
+            label_winner_shimocha = tk.Label(self.root,text="下家",font=("Helvetica", 14, "bold"))
+            label_winner_shimocha.place(x=10, y=winner_player_y)
+            winner_player_y += 25
+            winner_score_board()
+            winner_player_y += 25
+        def winner_score_board():
+            global winner_player_y
+            han_count = tk.StringVar()
+            han_1 = ttk.Radiobutton(self.root, text="1", variable=han_count, value=1, style="Winner.TRadiobutton")
+            han_2 = ttk.Radiobutton(self.root, text="2", variable=han_count, value=2, style="Winner.TRadiobutton")
+            han_3 = ttk.Radiobutton(self.root, text="3", variable=han_count, value=3, style="Winner.TRadiobutton")
+            han_4 = ttk.Radiobutton(self.root, text="4", variable=han_count, value=4, style="Winner.TRadiobutton")
+            han_mangan = ttk.Radiobutton(self.root, text="满贯", variable=han_count, value="满贯",
+                                         style="Winner.TRadiobutton")
+            han_haneman = ttk.Radiobutton(self.root, text="跳满", variable=han_count, value="跳满",
+                                          style="Winner.TRadiobutton")
+            han_baiman = ttk.Radiobutton(self.root, text="倍满", variable=han_count, value="倍满",
+                                         style="Winner.TRadiobutton")
+            han_sanbaiman = ttk.Radiobutton(self.root, text="三倍满", variable=han_count, value="三倍满",
+                                            style="Winner.TRadiobutton")
+            han_yakuman = ttk.Radiobutton(self.root, text="役满", variable=han_count, value="役满",
+                                          style="Winner.TRadiobutton")
+            han_1.place(x=0,y=winner_player_y)
+            han_2.place(x=30, y=winner_player_y)
+            han_3.place(x=60, y=winner_player_y)
+            han_4.place(x=90, y=winner_player_y)
+            han_mangan.place(x=120, y=winner_player_y)
+            han_haneman.place(x=170, y=winner_player_y)
+            han_baiman.place(x=220, y=winner_player_y)
+            han_sanbaiman.place(x=270, y=winner_player_y)
+            han_yakuman.place(x=340, y=winner_player_y)
+
+            winner_player_y += 25
+
+            fu_count = tk.StringVar()
+            fu_null = ttk.Radiobutton(self.root,text="-",variable=fu_count,value=0,style="Winner.TRadiobutton")
+            fu_20 = ttk.Radiobutton(self.root,text=20,variable=fu_count,value=20,style="Winner.TRadiobutton")
+            fu_30 = ttk.Radiobutton(self.root,text=30,variable=fu_count,value=30,style="Winner.TRadiobutton")
+            fu_40 = ttk.Radiobutton(self.root,text=40,variable=fu_count,value=40,style="Winner.TRadiobutton")
+
+            fu_20.place(x=0,y=winner_player_y)
+            fu_30.place(x=40,y=winner_player_y)
+            fu_40.place(x=80,y=winner_player_y)
+            fu_null.place(x=120,y=winner_player_y)
+
+
+
+
         loser = tk.StringVar()
         loser_self = ttk.Radiobutton(self.root, text="自家", variable=loser, value="self", style="Method.TRadiobutton")
         loser_kamicha = ttk.Radiobutton(self.root, text="上家", variable=loser, value="kamicha",
@@ -43,8 +124,19 @@ class changeScore:
         label_deal_to = tk.Label(self.root, text="放铳至", font=("Helvetica", 20, "bold"))
         label_deal_to.place(x=10, y=90)
 
-        winner_list = []
+        winner_self = tk.Button(self.root, text="自家", font=("Helvetica", 20, "bold"), command=click_winner_self)
+        winner_kamicha = tk.Button(self.root, text="上家", font=("Helvetica", 20, "bold"), command=click_winner_kamicha)
+        winner_toimen = tk.Button(self.root, text="对家", font=("Helvetica", 20, "bold"), command=click_winner_toimen)
+        winner_shimo = tk.Button(self.root, text="下家", font=("Helvetica", 20, "bold"), command=click_winner_shimocha)
 
+        winner_self.place(x=10, y=120)
+        winner_kamicha.place(x=90, y=120)
+        winner_toimen.place(x=170, y=120)
+        winner_shimo.place(x=250, y=120)
+
+
+
+        winner_list = []
 
     def click_button_zimo(self):
         pass
